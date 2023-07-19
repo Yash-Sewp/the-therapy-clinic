a612181bdbead323a5eea579156874ac0268ff8a
Yash-Sewp <yashveersewparthab@gmail.com>
2023-07-19 21:27:03 +0200

    contact page

--- src/app/pages/contact/contact.component.ts
+++ src/app/pages/contact/contact.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import * as AOS from 'aos'; //AOS - 1
 
 @Component({
   selector: 'app-contact',
@@ -6,5 +7,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./contact.component.scss']
 })
 export class ContactComponent {
+  ngOnInit() {
+    AOS.init();
+  }
 
+  ngAfterViewInit(): void {
+    setTimeout(() => {
+      AOS.refresh()
+    }, 500)
+  }
 }
