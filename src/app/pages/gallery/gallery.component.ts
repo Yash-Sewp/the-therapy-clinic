ecb5700bdc9ceb85c5b37622a60a704e2d623e93
Yash-Sewp <yashveersewparthab@gmail.com>
2023-12-22 18:16:04 +0200

    gallery fix

--- src/app/pages/gallery/gallery.component.ts
+++ src/app/pages/gallery/gallery.component.ts
@@ -8,8 +8,8 @@ import * as AOS from 'aos'; //AOS - 1
   styleUrls: ['./gallery.component.scss']
 })
 export class GalleryComponent {
-  
-  constructor(private title: Title, private meta: Meta) {}
+
+  constructor(private title: Title, private meta: Meta) { }
 
   ngOnInit(): void {
     AOS.init();
@@ -36,53 +36,11 @@ export class GalleryComponent {
     }
   ];
 
-  occupationalRoom: any = [
-    {
-      "img": "assets/images/gallery/OT Room.png",
-    },
-    {
-      "img": "assets/images/gallery/OT Room (2).png",
-    }
-  ];
+  occupationalRoom: any = [];
 
-  traumaCounselling: any = [
-    {
-      "img": "assets/images/gallery/TC Room.png",
-    },
-    {
-      "img": "assets/images/gallery/TC Room (2).png",
-    }
-  ];
+  traumaCounselling: any = [];
 
-  sensoryRoom: any = [
-    {
-      "img": "assets/images/gallery/SI Room.png",
-    },
-    {
-      "img": "assets/images/gallery/SI Room (2).png",
-    },
-    {
-      "img": "assets/images/gallery/SI Room (3).png",
-    },
-    {
-      "img": "assets/images/gallery/SI Room (4).png",
-    },
-    {
-      "img": "assets/images/gallery/SI Room (5).png",
-    },
-    {
-      "img": "assets/images/gallery/SI Room (6).png",
-    },
-    {
-      "img": "assets/images/gallery/SI Room (6).png",
-    },
-    {
-      "img": "assets/images/gallery/SI Room (7).png",
-    },
-    {
-      "img": "assets/images/gallery/SI Room (8).png",
-    }
-  ];
+  sensoryRoom: any = [];
 
   practiceConfig = {
     "slidesToShow": 1,
@@ -107,9 +65,59 @@ export class GalleryComponent {
   };
 
   tabChanged(event: any): void {
-    if(event === 1) {
-    
-    } 
+    if (event === 1) {
+      setTimeout(() => {
+        this.occupationalRoom.push({
+          "img": "assets/images/gallery/OT Room.png",
+        },
+          {
+            "img": "assets/images/gallery/OT Room (2).png",
+          });
+      }, 500);
+    } else if (event === 2) {
+      setTimeout(() => {
+        this.traumaCounselling.push(
+          {
+            "img": "assets/images/gallery/TC Room.png",
+          },
+          {
+            "img": "assets/images/gallery/TC Room (2).png",
+          }
+        )
+      }, 500);
+    } else if (event === 3) {
+      setTimeout(() => {
+        this.sensoryRoom.push(
+          {
+            "img": "assets/images/gallery/SI Room.png",
+          },
+          {
+            "img": "assets/images/gallery/SI Room (2).png",
+          },
+          {
+            "img": "assets/images/gallery/SI Room (3).png",
+          },
+          {
+            "img": "assets/images/gallery/SI Room (4).png",
+          },
+          {
+            "img": "assets/images/gallery/SI Room (5).png",
+          },
+          {
+            "img": "assets/images/gallery/SI Room (6).png",
+          },
+          {
+            "img": "assets/images/gallery/SI Room (6).png",
+          },
+          {
+            "img": "assets/images/gallery/SI Room (7).png",
+          },
+          {
+            "img": "assets/images/gallery/SI Room (8).png",
+          }
+        )
+      }, 500);
+    }
     // Do something when tabs are changed, for example, update data or perform an action.
   }
 
